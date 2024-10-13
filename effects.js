8aafb9188a94c327342e7763b687ee0430849a47
muwudev <cavitieshurt@gmail.com>
2024-10-14 02:55:49 +0530

    fixed static image

--- effects.js
+++ effects.js
@@ -1,13 +1,18 @@
 // function to pause GIF when not hovering and play when hovering
 
-document.addEventListener("DOMContentLoaded", function () {
-    const gifImage = document.querySelector(".hover-gif");
-  
-    gifImage.addEventListener("mouseenter", function () {
-      gifImage.src = "images/Lighthouse Follower-animation-big.gif"; // When hovering, display the GIF
-    });
-  
-    gifImage.addEventListener("mouseleave", function () {
-      gifImage.src = "images/Lighthouse Follower-still-big.png"; // When not hovering, display the static image
-    });
+document.addEventListener("DOMContentLoaded", function () { 
+  const gifImage = document.querySelector(".hover-gif");
+
+  // Set the default static image when the page loads
+  gifImage.src = "images/Lighthouse Follower-still-big.png";
+
+  gifImage.addEventListener("mouseenter", function () {
+    // Change the image source to the GIF when hovering
+    gifImage.src = "images/Lighthouse Follower-animation-big.gif";
   });
+
+  gifImage.addEventListener("mouseleave", function () {
+    // Revert back to the static image when not hovering
+    gifImage.src = "images/Lighthouse Follower-still-big.png";
+  });
+});
